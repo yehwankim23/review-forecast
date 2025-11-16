a483558fd5a7edca847e0bee3fecc5e0cd5f3d3d
김예환 Ye-Hwan Kim (Sam) <yehwankim23@gmail.com>
2025-11-16 23:47:19 +0900

    Create page

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
+import { fileURLToPath, URL } from "node:url";
+
+export default defineConfig({
+  plugins: [vue(), vueDevTools()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
